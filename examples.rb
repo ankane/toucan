690ab2ead00cedd2796e56bc68d70d6174abe202
Andrew Kane <andrew@getformidable.com>
2011-06-22 00:45:38 -0700

    Added days to counters and count methods for all periods

--- examples.rb
+++ examples.rb
@@ -3,5 +3,7 @@ require_relative "toucan"
 add_event(Time.now.to_i, "url", ["com", "amazon", "music"], 1)
 add_event(Time.now.to_i, "url", ["com", "facebook", "www"], 1)
 
-print_counts("com")
-print_counts("com.amazon")
+minute_counts("com")
+hour_counts("com")
+day_counts("com")
+lifetime_counts("com")
--- toucan.rb
+++ toucan.rb
@@ -10,14 +10,16 @@ def add_event(timestamp, category, key, value, properties = {}, properties_with_
     pairs[key.join(".")] = value
   end while key.pop and !key.empty?
 
-  # no time buckets for now
-
   pairs.each do |key, val|
     $redis.incrby("lifetime:#{key}", val)
     hour = round_time(timestamp, 3600)
     $redis.incrby("hour.#{hour}:#{key}", val)
     minute = round_time(timestamp, 60)
     $redis.incrby("minute.#{minute}:#{key}", val)
+    # use date for day YYYY-MM-DD instead of timestamp
+    # need to account for time zone
+    day = Time.at(timestamp).to_date.strftime("%Y-%m-%d")
+    $redis.incrby("day.#{day}:#{key}", val)
   end
 end
 
@@ -25,15 +27,37 @@ def round_time(timestamp, seconds = 60)
   (timestamp.to_f / seconds).floor * seconds
 end
 
-def print_counts(key)
-  last = round_time(Time.now, 60)
-  first = last - 300
-  steps = *(first..last).step(60)
+def minute_counts(key, seconds = 60, prefix = "minute")
+  last = round_time(Time.now, seconds)
+  first = last - 9 * seconds
+  steps = *(first..last).step(seconds)
 
-  keys = steps.map{|step| "minute.#{step}:#{key}"}
+  count_helper(key, prefix, steps)
+end
+
+def hour_counts(key)
+  minute_counts(key, 3600, "hour")
+end
+
+def day_counts(key)
+  last = Date.today
+  first = last - 9
+  steps = *first.step(last)
+
+  count_helper(key, "day", steps)
+end
+
+def lifetime_counts(key)
+  value = $redis.get("lifetime:#{key}")
+
+  puts "lifetime - #{key} - #{value}"
+end
+
+def count_helper(key, prefix, steps)
+  keys = steps.map{|step| "#{prefix}.#{step}:#{key}"}
 
   # get keys and replace nils with zero
   values = $redis.mget(*keys).map{|val| (val || 0).to_i }
 
-  puts "#{key}: #{values.inspect}"
+  puts "#{prefix} - #{key} - #{values.inspect}"
 end
