7d67dfe02c31fe19c0566faf6d7236c115dfd770
Andrew Kane <andrew@getformidable.com>
2011-06-22 01:05:53 -0700

    Moved library code into class

--- examples.rb
+++ examples.rb
@@ -1,20 +1,23 @@
 require_relative "toucan"
+require "pp"
 
-add_event(Time.now.to_i, "url", ["com", "amazon", "music"], 1)
-add_event(Time.now.to_i, "url", ["com", "facebook", "www"], 1)
+toucan = Toucan.new
+
+toucan.add_event(Time.now.to_i, "url", ["com", "amazon", "music"], 1)
+toucan.add_event(Time.now.to_i, "url", ["com", "facebook", "www"], 1)
 
 puts "minute - com"
-pp minute_counts("com")
+pp toucan.minute_counts("com")
 puts
 
 puts "hour - com.amazon"
-pp hour_counts("com.amazon")
+pp toucan.hour_counts("com.amazon")
 puts
 
 puts "day - com - 5 points"
-pp day_counts("com", 5)
+pp toucan.day_counts("com", 5)
 puts
 
 puts "lifetime - com.facebook.www"
-pp lifetime_counts("com.facebook.www")
+pp toucan.lifetime_counts("com.facebook.www")
 puts
--- toucan.rb
+++ toucan.rb
@@ -1,62 +1,70 @@
 require "rubygems"
 require "bundler/setup"
 require "redis"
-require "pp"
 
-$redis = Redis.new
+class Toucan
 
-def add_event(timestamp, category, key, value, properties = {}, properties_with_counts = {})
-  pairs = {}
-  begin
-    pairs[key.join(".")] = value
-  end while key.pop and !key.empty?
-
-  pairs.each do |key, val|
-    $redis.incrby("lifetime:#{key}", val)
-    hour = round_time(timestamp, 3600)
-    $redis.incrby("hour.#{hour}:#{key}", val)
-    minute = round_time(timestamp, 60)
-    $redis.incrby("minute.#{minute}:#{key}", val)
-    # use date for day YYYY-MM-DD instead of timestamp
-    # need to account for time zone
-    day = Time.at(timestamp).to_date.strftime("%Y-%m-%d")
-    $redis.incrby("day.#{day}:#{key}", val)
+  def initialize
+    @redis = Redis.new
   end
-end
 
-def round_time(timestamp, seconds = 60)
-  (timestamp.to_f / seconds).floor * seconds
-end
+  def add_event(timestamp, category, key, value, properties = {}, properties_with_counts = {})
+    pairs = {}
+    begin
+      pairs[key.join(".")] = value
+    end while key.pop and !key.empty?
 
-def minute_counts(key, points = 10, seconds = 60, prefix = "minute")
-  last = round_time(Time.now, seconds)
-  first = last - (points - 1) * seconds
-  steps = *(first..last).step(seconds)
+    pairs.each do |key, val|
+      @redis.incrby("lifetime:#{key}", val)
+      hour = round_time(timestamp, 3600)
+      @redis.incrby("hour.#{hour}:#{key}", val)
+      minute = round_time(timestamp, 60)
+      @redis.incrby("minute.#{minute}:#{key}", val)
+      # use date for day YYYY-MM-DD instead of timestamp
+      # need to account for time zone
+      day = Time.at(timestamp).to_date.strftime("%Y-%m-%d")
+      @redis.incrby("day.#{day}:#{key}", val)
+    end
+  end
 
-  count_helper(key, prefix, steps)
-end
+  def minute_counts(key, points = 10, seconds = 60, prefix = "minute")
+    last = round_time(Time.now, seconds)
+    first = last - (points - 1) * seconds
+    steps = *(first..last).step(seconds)
 
-def hour_counts(key, points = 10)
-  minute_counts(key, points, 3600, "hour")
-end
+    count_helper(key, prefix, steps)
+  end
 
-def day_counts(key, points = 10)
-  last = Date.today
-  first = last - (points - 1)
-  steps = *first.step(last)
+  def hour_counts(key, points = 10)
+    minute_counts(key, points, 3600, "hour")
+  end
 
-  count_helper(key, "day", steps)
-end
+  def day_counts(key, points = 10)
+    last = Date.today
+    first = last - (points - 1)
+    steps = *first.step(last)
 
-def lifetime_counts(key)
-  $redis.get("lifetime:#{key}").to_i
-end
+    count_helper(key, "day", steps)
+  end
 
-def count_helper(key, prefix, steps)
-  keys = steps.map{|step| "#{prefix}.#{step}:#{key}"}
+  def lifetime_counts(key)
+    @redis.get("lifetime:#{key}").to_i
+  end
 
-  # get keys and replace nils with zero
-  values = $redis.mget(*keys).map{|val| (val || 0).to_i }
+  protected
 
-  Hash[steps.map{|step| step = step.strftime("%Y-%m-%d") if step.is_a?(Date); [step, values.shift]}]
+  def count_helper(key, prefix, steps)
+    keys = steps.map{|step| "#{prefix}.#{step}:#{key}"}
+
+    # get keys and replace nils with zero
+    values = @redis.mget(*keys).map{|val| (val || 0).to_i }
+
+    Hash[steps.map{|step| step = step.strftime("%Y-%m-%d") if step.is_a?(Date); [step, values.shift]}]
+  end
+
+  # http://stackoverflow.com/questions/449271/how-to-round-a-time-down-to-the-nearest-15-minutes-in-ruby/449322#449322
+  def round_time(timestamp, seconds = 60)
+    (timestamp.to_f / seconds).floor * seconds
+  end
+
 end
